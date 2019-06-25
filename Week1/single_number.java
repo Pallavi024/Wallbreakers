0d909c2297ddf4a35e1b6c5ed3c4053fc91843db
Pallavi <pallavibishnoi24@gmail.com>
2019-06-25 14:55:42 +0530

    solved Single Number problem

--- Week1/single_number.java
+++ Week1/single_number.java
@@ -1,19 +1,9 @@
 class Solution {
-    public int binaryGap(int N) {
-     
-        ArrayList<Integer> arr = new ArrayList<>();
-        int k = 1;
-        while(N > 0)
-        {
-            if(N%2 == 1)
-              arr.add(k);
-            N = N/2;
-            k++;
-        }
-        int diff = 0;
-        for(int i = 0;i<arr.size() - 1;i++)
-            diff = Math.max(diff,arr.get(i + 1) - arr.get(i));
-    
-        return diff;
+    public int singleNumber(int[] nums) {
+        
+        int xor = 0;
+        for(int i=0;i<nums.length;i++)
+            xor = xor ^ nums[i];
+        return xor;
     }
 }
