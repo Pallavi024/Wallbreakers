ebe19ff9a5243d5343a055358b480579f24d6f02
Pallavi <pallavibishnoi24@gmail.com>
2019-07-05 19:18:52 +0530

    solved Jewels and Stones problem

---
+++ Week2/jewels_and_stones.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+     
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i=0;i<S.length();i++)
+        {
+            if(map.containsKey(S.charAt(i)))
+                map.put(S.charAt(i), map.get(S.charAt(i)) + 1);
+            else
+                map.put(S.charAt(i), 1);
+        }
+        int count = 0;
+        for(int i = 0; i< J.length();i++)
+        {
+            if(map.containsKey(J.charAt(i)))
+                count+=map.get(J.charAt(i));
+        }
+        return count;
+    }
+}
